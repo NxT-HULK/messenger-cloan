4fd92de54ccf9722e1ba1f9ab399fad7dacab74f
Shivam Kumar Kashyap <109240141+MernWala@users.noreply.github.com>
2024-03-08 19:42:39 +0530

    add_on: conversation body comleted

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -10,17 +10,8 @@ const Avatar: React.FC<AvatarProps> = ({
     user
 }) => {
     return (
-        <div className="relative">
-            <div className={`
-                relative
-                inline-block
-                rounded-full
-                overflow-hidden
-                h-9
-                w-9
-                md:h-11
-                md:w-11
-            `}>
+        <div className="relative flex items-center">
+            <div className={`relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11`}>
                 <Image alt='avatar' src={user?.image || '/images/placeholder.jpg'}
                     fill
                 />
